ab6292eb516cee4618c492c0303d8ff7643cea11
SayuZX <todraihan95@gmail.com>
2023-07-14 01:43:02 +0700

    adapt for scandium

---
+++ common/sepolicy.mk
@@ -0,0 +1,35 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Lineage
+#
+
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+ifeq ($(TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR),true)
+BOARD_SEPOLICY_M4DEFS += board_excludes_fuseblk_sepolicy=true
+endif
+endif
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/scandium/sepolicy/common/public
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/scandium/sepolicy/common/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/scandium/sepolicy/common/dynamic \
+    device/scandium/sepolicy/common/system
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/scandium/sepolicy/common/dynamic \
+    device/scandium/sepolicy/common/vendor
+endif
+
+# Selectively include legacy rules defined by the products
+-include device/scandium/sepolicy/legacy-common/sepolicy.mk
